32b62d738781de0e106d967c6e1f42fb6062d256
zrheaume <zachary.a.rheaume@gmail.com>
2019-02-17 14:47:13 -0500

    Added (theoretically) heroku-deployable start script?

---
+++ server/client/precache-manifest.24043ed9fcde04b3df7dbbc09817ec37.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "89589cc29a63cc64c027",
+    "url": "/static/js/main.89589cc2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "54094b48b410ee26b80e",
+    "url": "/static/js/2.54094b48.chunk.js"
+  },
+  {
+    "revision": "7d60aeed814052a88caba993e47e0de2",
+    "url": "/index.html"
+  }
+];
