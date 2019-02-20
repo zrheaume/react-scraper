93ff6b13d8d6d6bd0ec6e9ac6e1c2a9f79d63607
zrheaume <zachary.a.rheaume@gmail.com>
2019-02-19 22:52:39 -0500

    Fixed connection issue

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -1,7 +1,7 @@
 import "dotenv/config"
 import mongooose from "mongoose"
 
-const connectionStr = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_SECRET}@ds139295.mlab.com:39295/heroku_spj8wq3x`
+const connectionStr = process.env.MONGODB_URI || `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_SECRET}@ds139295.mlab.com:39295/heroku_spj8wq3x`
 
 export default async function connect() {
    return new Promise(async function (resolve, reject) {
